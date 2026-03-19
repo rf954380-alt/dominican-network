5a52be5afe19ae5eb6313238fe4c4e84be3a26d3
rf954380-alt <rf954380@gmail.com>
2026-03-18 21:23:50 -0400

    Add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+
+let host = location.protocol + '//' + location.host;
+
+let _CONFIG = {
+  wispurl: localStorage.getItem('proxServer') || (location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host + '/wisp/',
+  bareurl: host + '/bare/'
+};
